89f7430f9c074a209caa6c99572a91ff892623aa
Mariano Bertoni <mariano.bertoni@gmail.com>
2023-09-23 13:52:00 -0300

    terminando de terminar seek n destroy,  testeala!

--- src/CEK.hs
+++ src/CEK.hs
@@ -2,34 +2,40 @@ module CEK where
 
 import Lang
 import MonadFD4
-import Common 
+import Common
+import Eval (semOp)
 
 data Val = VConst Const | VClosure Closure
+    deriving Show
+
+val2TTerm :: Val -> TTerm
+val2TTerm (VConst c) = Const (NoPos, NatTy) c
+val2TTerm (VClosure f) = abort "por ahora no tenemos operadores de alto orden, Seba hacete el apply y el comp"
 
 data Closure =
     ClosureFun Env Name TTerm
     | ClosureFix Env Name Name TTerm
+    deriving Show
 
 type Env = [Val] -- rho
 
 type Kont = [Frame]
-data Frame  = KArgL Env TTerm   -- (□ u)
-            | KArgR Closure     -- (t □)
-            | KClos Closure
-            | KIfz Env TTerm TTerm
-            | KBinL Env BinaryOp TTerm -- (□ + u)
-            | KBinR Env BinaryOp TTerm -- (t + □)
+data Frame  = KArgL Env TTerm          -- (App □ arg)
+            | KArgR Closure            -- (App f □)
+            | KIfz Env TTerm TTerm     -- (IfZ □ then else)
+            | KBinL Env BinaryOp TTerm -- (□ (+) u)
+            | KBinR BinaryOp Val       -- (v (+) □)
             | KVar Var
-            | KPrint String
-            | KLet Env Name TTerm
+            | KPrint String            -- (print s □)
+            | KLetDef Env Name TTerm   -- let □ in term
+            -- | KLetBody Env Name TTerm  -- let def in □
+            deriving Show
 
-
-seek :: MonadFD4 m => TTerm -> Env -> Kont -> m Val -- <>
--- < print s t , ρ, k> -> <t , ρ, (print s □):k >
-seek (Print _ s t) env k        = seek t env ((KPrint s):k) -- La info se pierde, supongo
-seek (BinaryOp i op t u) env k  = seek t env ((KBinL env op u):k)
-seek (IfZ i c t e) env k        = seek c env ((KIfz env t e):k)
-seek (App i t u) env k          = seek t env ((KArgL env u):k)
+seek :: MonadFD4 m => TTerm -> Env -> Kont -> m Val
+seek (Print _ s t) env k = seek t env (KPrint s:k) -- < print s t , ρ, k> -> <t , ρ, (print s □):k >
+seek (BinaryOp i op t u) env k  = seek t env (KBinL env op u : k)
+seek (IfZ i c t e) env k        = seek c env (KIfz env t e : k)
+seek (App i t u) env k          = seek t env (KArgL env u : k)
 seek (V i (Bound b)) env k      = abort "unimplemented" -- acá qué hay que hacer?
 seek (V i (Free nm)) env k      = abort "unimplemented" -- entiendo que acá tendríamos que fallar
 seek (V i (Global nm)) env k    = do
@@ -40,23 +46,28 @@ seek (V i (Global nm)) env k    = do
 seek (Lam i nm _ (Sc1 t)) env k     = destroy (VClosure (ClosureFun env nm t)) k
 seek (Fix i f _ x _ (Sc2 t)) env k  = destroy (VClosure (ClosureFix env f x t)) k
 seek (Const i c) env k = destroy (VConst c) k
-seek _ env k = abort "hacer el ejercicio de la practica"
+seek (Let i n _ def (Sc1 t)) env k = seek def env (KLetDef env n t : k)
+-- seek _ env k = abort "hacer el ejercicio de la practica"
 -- resta hacer el let
 
 destroy :: MonadFD4 m => Val -> Kont -> m Val       -- <<>>
 destroy v [] = return v
-destroy v ((KPrint str):k)          = destroy v k -- Nos falta imprimir?
-destroy n ((KBinL env op u):k)      = seek u env ((KBinR env op n):k)
-destroy n' ((KBinR env op n):k)     = destroy (N op n n') k
-destroy (VConst 0) ((KIfz env t e):k)    = seek t env k
-destroy (VConst _) ((KIfz env t e):k)    = seek e env k
-destroy (KClos clos) ((KArgL env t):k)                  = seek t env ((KClos clos):k)
-destroy v ((KArgR (ClosureFun env x t)):k)            = seek t (v:env) k
-destroy v ((KArgR clos@(ClosureFix env f x t) ):k)    = seek t ((Cnv clos):v:e) k
--- resta hacer el let
+destroy v (KPrint str : k)                                 = destroy v k -- Nos falta imprimir?
+destroy v (KBinL env op rt : k)                             = seek rt env (KBinR op v :k)
+destroy v (KBinR op lv : k)   = case (lv, v) of
+    (VConst (CNat l), VConst (CNat r)) -> destroy (VConst (CNat (semOp op l r))) k
+    _ -> abort "error de tipos runtime"
+destroy (VConst (CNat 0)) (KIfz env t e : k)               = seek t env k
+destroy (VConst (CNat _)) (KIfz env t e : k)               = seek e env k
+destroy (VClosure clos) (KArgL env t : k)                  = seek t env (KArgR clos : k)
+destroy v (KArgR (ClosureFun env x t) : k)                 = seek t (v : env) k
+destroy v (KArgR clos@(ClosureFix env f x t) : k)          = seek t (VClosure clos : v : env) k
+destroy v (KLetDef env nm body : k)                        = seek body (v : env) k
+-- destroy v (KLetBody)                                       = _
+destroy v _ = abort "its no possible Blenda"
 
-{-
-eval :: MonadFD4 m => TTerm -> m TTerm
-eval t = do v <- seek t [] []
-            return (valToTTerm v)
--}
+
+evalCEK :: MonadFD4 m => TTerm -> m TTerm
+evalCEK t = do
+    v <- seek t [] []
+    return (val2TTerm v)
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
--- {-# LANGUAGE OverloadedRecordDot #-}
 
+-- {-# LANGUAGE OverloadedRecordDot #-}
 {-|
 Module      : Main
 Description : Compilador de FD4.
@@ -10,74 +10,79 @@ Maintainer  : mauro@fceia.unr.edu.ar
 Stability   : experimental
 
 -}
-
 module Main where
 
-import System.Console.Haskeline ( defaultSettings, getInputLine, runInputT, InputT )
-import Control.Monad.Catch (MonadMask)
-
+import           System.Console.Haskeline (defaultSettings, getInputLine
+                                         , runInputT, InputT)
+import           Control.Monad.Catch (MonadMask)
 --import Control.Monad
-import Control.Monad.Trans
-import Data.List (nub, isPrefixOf, intercalate )
-import Data.Char ( isSpace )
-import Control.Exception ( catch , IOException )
-import System.IO ( hPrint, stderr, hPutStrLn )
-import Data.Maybe ( fromMaybe )
-
-import System.Exit ( exitWith, ExitCode(ExitFailure) )
-import Options.Applicative
-
-import Global
-import Errors
-import Lang
-import Parse ( P, tm, program, declOrTm, runP )
-import Elab ( elabDeclaration , elabTerm)
-import Eval ( eval )
-import PPrint ( pp, ppTy, ppTypeDecl, ppTermDecl )
-import MonadFD4
-import TypeChecker ( tc, tcDecl )
-import Common ()
+import           Control.Monad.Trans
+import           Data.List (nub, isPrefixOf, intercalate)
+import           Data.Char (isSpace)
+import           Control.Exception (catch, IOException)
+import           System.IO (hPrint, stderr, hPutStrLn)
+import           Data.Maybe (fromMaybe)
+import           System.Exit (exitWith, ExitCode(ExitFailure))
+import           Options.Applicative
+import           Global
+import           Errors
+import           Lang
+import           Parse (P, tm, program, declOrTm, runP)
+import           Elab (elabDeclaration, elabTerm)
+import           Eval (eval)
+import           PPrint (pp, ppTy, ppTypeDecl, ppTermDecl)
+import           MonadFD4
+import           TypeChecker (tc, tcDecl)
+import           Common (abort)
+import CEK (evalCEK)
 
 prompt :: String
 prompt = "FD4> "
 
-
-
 -- | Parser de banderas
-parseMode :: Parser (Mode,Bool)
-parseMode = (,) <$>
-      (flag' Typecheck ( long "typecheck" <> short 't' <> help "Chequear tipos e imprimir el término")
-  -- <|> flag' InteractiveCEK (long "interactiveCEK" <> short 'k' <> help "Ejecutar interactivamente en la CEK")
-  -- <|> flag' Bytecompile (long "bytecompile" <> short 'm' <> help "Compilar a la BVM")
-  -- <|> flag' RunVM (long "runVM" <> short 'r' <> help "Ejecutar bytecode en la BVM")
-      <|> flag Interactive Interactive ( long "interactive" <> short 'i' <> help "Ejecutar en forma interactiva")
-      <|> flag Eval        Eval        (long "eval" <> short 'e' <> help "Evaluar programa")
+parseMode :: Parser (Mode, Bool)
+parseMode = (,)
+  <$> (flag'
+         Typecheck
+         (long "typecheck"
+          <> short 't'
+          <> help "Chequear tipos e imprimir el término")
+        <|> flag' InteractiveCEK (long "interactiveCEK" <> short 'k' <> help "Ejecutar interactivamente en la CEK")
+       -- <|> flag' Bytecompile (long "bytecompile" <> short 'm' <> help "Compilar a la BVM")
+       -- <|> flag' RunVM (long "runVM" <> short 'r' <> help "Ejecutar bytecode en la BVM")
+       <|> flag
+         Interactive
+         Interactive
+         (long "interactive"
+          <> short 'i'
+          <> help "Ejecutar en forma interactiva")
+       <|> flag Eval Eval (long "eval" <> short 'e' <> help "Evaluar programa"))
   -- <|> flag' CC ( long "cc" <> short 'c' <> help "Compilar a código C")
   -- <|> flag' Canon ( long "canon" <> short 'n' <> help "Imprimir canonicalización")
   -- <|> flag' Assembler ( long "assembler" <> short 'a' <> help "Imprimir Assembler resultante")
   -- <|> flag' Build ( long "build" <> short 'b' <> help "Compilar")
-      )
-   <*> pure False
+  <*> pure False
+
    -- reemplazar por la siguiente línea para habilitar opción
    -- <*> flag False True (long "optimize" <> short 'o' <> help "Optimizar código")
-
 -- | Parser de opciones general, consiste de un modo y una lista de archivos a procesar
-parseArgs :: Parser (Mode,Bool, [FilePath])
-parseArgs = (\(a,b) c -> (a,b,c)) <$> parseMode <*> many (argument str (metavar "FILES..."))
+parseArgs :: Parser (Mode, Bool, [FilePath])
+parseArgs = (\(a, b) c -> (a, b, c)) <$> parseMode
+  <*> many (argument str (metavar "FILES..."))
 
 main :: IO ()
 main = execParser opts >>= go
   where
-    opts = info (parseArgs <**> helper)
-      ( fullDesc
-     <> progDesc "Compilador de FD4"
-     <> header "Compilador de FD4 de la materia Compiladores 2023" )
+    opts = info
+      (parseArgs <**> helper)
+      (fullDesc
+       <> progDesc "Compilador de FD4"
+       <> header "Compilador de FD4 de la materia Compiladores 2023")
 
-    go :: (Mode,Bool,[FilePath]) -> IO ()
-    go (Interactive,opt,files) =
-              runOrFail (Conf opt Interactive) (runInputT defaultSettings (repl files))
-    go (m,opt, files) =
-              runOrFail (Conf opt m) $ mapM_ compileFile files
+    go :: (Mode, Bool, [FilePath]) -> IO ()
+    go (Interactive, opt, files) =
+      runOrFail (Conf opt Interactive) (runInputT defaultSettings (repl files))
+    go (m, opt, files) = runOrFail (Conf opt m) $ mapM_ compileFile files
 
 runOrFail :: Conf -> FD4 a -> IO a
 runOrFail c m = do
@@ -86,100 +91,111 @@ runOrFail c m = do
     Left err -> do
       liftIO $ hPrint stderr err
       exitWith (ExitFailure 1)
-    Right v -> return v
+    Right v  -> return v
 
 repl :: (MonadFD4 m, MonadMask m) => [FilePath] -> InputT m ()
 repl args = do
-       lift $ setInter True
-       lift $ catchErrors $ mapM_ compileFile args
-       s <- lift get
-       when (inInteractiveMode s) $ liftIO $ putStrLn
-         (  "Entorno interactivo para FD4.\n"
-         ++ "Escriba :? para recibir ayuda.")
-       loop
-  where loop = do
-           minput <- getInputLine prompt
-           case minput of
-               Nothing -> return ()
-               Just "" -> loop
-               Just x -> do
-                       c <- liftIO $ interpretCommand x
-                       b <- lift $ catchErrors $ handleCommand c
-                       maybe loop (`when` loop) b
+  lift $ setInter True
+  lift $ catchErrors $ mapM_ compileFile args
+  s <- lift get
+  when (inInteractiveMode s)
+    $ liftIO
+    $ putStrLn
+      ("Entorno interactivo para FD4.\n" ++ "Escriba :? para recibir ayuda.")
+  loop
+  where
+    loop = do
+      minput <- getInputLine prompt
+      case minput of
+        Nothing -> return ()
+        Just "" -> loop
+        Just x  -> do
+          c <- liftIO $ interpretCommand x
+          b <- lift $ catchErrors $ handleCommand c
+          maybe loop (`when` loop) b
 
-loadFile ::  MonadFD4 m => FilePath -> m [SDeclaration]
+loadFile :: MonadFD4 m => FilePath -> m [SDeclaration]
 loadFile f = do
-    let filename = reverse (dropWhile isSpace (reverse f))
-    x <- liftIO $ catch (readFile filename)
-               (\e -> do let err = show (e :: IOException)
-                         hPutStrLn stderr ("No se pudo abrir el archivo " ++ filename ++ ": " ++ err)
-                         return "")
-    setLastFile filename
-    parseIO filename program x
+  let filename = reverse (dropWhile isSpace (reverse f))
+  x <- liftIO
+    $ catch
+      (readFile filename)
+      (\e -> do
+         let err = show (e :: IOException)
+         hPutStrLn
+           stderr
+           ("No se pudo abrir el archivo " ++ filename ++ ": " ++ err)
+         return "")
+  setLastFile filename
+  parseIO filename program x
 
-compileFile ::  MonadFD4 m => FilePath -> m ()
+compileFile :: MonadFD4 m => FilePath -> m ()
 compileFile f = do
-    i <- getInter
-    setInter False
-    when i $ printFD4 ("Abriendo "++f++"...")
-    declarations <- loadFile f
-    mapM_ handleDeclaration declarations
-    setInter i
+  i <- getInter
+  setInter False
+  when i $ printFD4 ("Abriendo " ++ f ++ "...")
+  declarations <- loadFile f
+  mapM_ handleDeclaration declarations
+  setInter i
 
-parseIO ::  MonadFD4 m => String -> P a -> String -> m a
+parseIO :: MonadFD4 m => String -> P a -> String -> m a
 parseIO filename p x = case runP p x filename of
-                  Left e  -> throwError (ParseErr e)
-                  Right r -> return r
+  Left e  -> throwError (ParseErr e)
+  Right r -> return r
 
 evalDecl :: MonadFD4 m => Decl TTerm -> m (Decl TTerm)
 evalDecl (Decl p x e) = do
-    e' <- eval e
-    return (Decl p x e')
+  e' <- eval e
+  return (Decl p x e')
 
-handleDeclaration ::  MonadFD4 m => SDeclaration -> m ()
+handleDeclaration :: MonadFD4 m => SDeclaration -> m ()
 handleDeclaration d = do
-        m <- getMode
-        s <- get
-        case m of
-          Interactive -> case elabDeclaration (globalTypeContext s) d of
-            Decl p x (Left term) -> do
-              tt <- tcDecl (Decl p x term)
-              te <- eval (declBody tt)
-              addTermDecl (Decl p x te)
-            Decl p x (Right ty) -> do
-              addTypeDecl (Decl p x ty)
-              
-          Typecheck -> do
-            f <- getLastFile
-            printFD4 ("Chequeando tipos de "++f)
-            case elabDeclaration (globalTypeContext s) d of
-              Decl p x (Left term) -> do
-                tt <- tcDecl (Decl p x term)
-                addTermDecl tt
-                ppterm <- ppTermDecl tt  
-                printFD4 ppterm
-              Decl p x (Right ty) -> do
-                addTypeDecl (Decl p x ty)
-                ppty <- ppTypeDecl (Decl p x ty)  
-                printFD4 ppty
-            -- opt <- getOpt
-            -- td' <- if opt then optimize td else td
-          
-          Eval -> case elabDeclaration (globalTypeContext s) d of
-            Decl p x (Left term) -> do
-              tt <- tcDecl (Decl p x term)
-              te <- eval (declBody tt)
-              addTermDecl (Decl p x te)
-            Decl p x (Right ty) -> do
-              addTypeDecl (Decl p x ty)
+  m <- getMode
+  s <- get
+  case m of
+    _ -> case elabDeclaration (globalTypeContext s) d of
+      Decl p x (Left term) -> do
+        tt <- tcDecl (Decl p x term)
+        te <- evalCEK (declBody tt)
+        addTermDecl (Decl p x te)
+      Decl p x (Right ty)  -> do
+        addTypeDecl (Decl p x ty)
+
+    Interactive -> case elabDeclaration (globalTypeContext s) d of
+      Decl p x (Left term) -> do
+        tt <- tcDecl (Decl p x term)
+        te <- eval (declBody tt)
+        addTermDecl (Decl p x te)
+      Decl p x (Right ty)  -> do
+        addTypeDecl (Decl p x ty)
+    Typecheck   -> do
+      f <- getLastFile
+      printFD4 ("Chequeando tipos de " ++ f)
+      case elabDeclaration (globalTypeContext s) d of
+        Decl p x (Left term) -> do
+          tt <- tcDecl (Decl p x term)
+          addTermDecl tt
+          ppterm <- ppTermDecl tt
+          printFD4 ppterm
+        Decl p x (Right ty)  -> do
+          addTypeDecl (Decl p x ty)
+          ppty <- ppTypeDecl (Decl p x ty)
+          printFD4 ppty
+      -- opt <- getOpt
+      -- td' <- if opt then optimize td else td
+    Eval        -> case elabDeclaration (globalTypeContext s) d of
+      Decl p x (Left term) -> do
+        tt <- tcDecl (Decl p x term)
+        te <- eval (declBody tt)
+        addTermDecl (Decl p x te)
+      Decl p x (Right ty)  -> do
+        addTypeDecl (Decl p x ty)
+
               -- do
               -- td <- typecheckDecl d
               -- -- td' <- if opt then optimizeDecl td else return td
               -- ed <- evalDecl td
               -- addDecl ed
-
-
-
 data Command = Compile CompileForm
              | PPrint String
              | Type String
@@ -189,109 +205,136 @@ data Command = Compile CompileForm
              | Help
              | Noop
 
-data CompileForm = CompileInteractive  String
-                 | CompileFile         String
+data CompileForm = CompileInteractive String
+                 | CompileFile String
 
 data InteractiveCommand = Cmd [String] String (String -> Command) String
 
 -- | Parser simple de comando interactivos
 interpretCommand :: String -> IO Command
-interpretCommand x
-  =  if ":" `isPrefixOf` x then
-       do  let  (cmd,t')  =  break isSpace x
-                t         =  dropWhile isSpace t'
-           --  find matching commands
-           let  matching  =  filter (\ (Cmd cs _ _ _) -> any (isPrefixOf cmd) cs) commands
-           case matching of
-             []  ->  do  putStrLn ("Comando desconocido `" ++ cmd ++ "'. Escriba :? para recibir ayuda.")
-                         return Noop
-             [Cmd _ _ f _]
-                 ->  do  return (f t)
-             _   ->  do  putStrLn ("Comando ambigüo, podría ser " ++
-                                   intercalate ", " ([ head cs | Cmd cs _ _ _ <- matching ]) ++ ".")
-                         return Noop
-
-     else
-       return (Compile (CompileInteractive x))
+interpretCommand x =
+  if ":" `isPrefixOf` x
+  then do
+    let (cmd, t') = break isSpace x
+        t = dropWhile isSpace t'
+    --  find matching commands
+    let matching = filter (\(Cmd cs _ _ _) -> any (isPrefixOf cmd) cs) commands
+    case matching of
+      [] -> do
+        putStrLn
+          ("Comando desconocido `"
+           ++ cmd
+           ++ "'. Escriba :? para recibir ayuda.")
+        return Noop
+      [Cmd _ _ f _] -> do
+        return (f t)
+      _ -> do
+        putStrLn
+          ("Comando ambiguo, podría ser "
+           ++ intercalate ", " ([head cs | Cmd cs _ _ _ <- matching])
+           ++ ".")
+        return Noop
+  else return (Compile (CompileInteractive x))
 
 commands :: [InteractiveCommand]
-commands
-  =  [ Cmd [":browse"]      ""        (const Browse) "Ver los nombres en scope",
-       Cmd [":load"]        "<file>"  (Compile . CompileFile)
-                                                     "Cargar un programa desde un archivo",
-       Cmd [":print"]       "<exp>"   PPrint          "Imprime un término y sus ASTs sin evaluarlo",
-       Cmd [":reload"]      ""        (const Reload)         "Vuelve a cargar el último archivo cargado",
-       Cmd [":type"]        "<exp>"   Type           "Chequea el tipo de una expresión",
-       Cmd [":quit",":Q"]        ""        (const Quit)   "Salir del intérprete",
-       Cmd [":help",":?"]   ""        (const Help)   "Mostrar esta lista de comandos" ]
+commands =
+  [ Cmd [":browse"] "" (const Browse) "Ver los nombres en scope"
+  , Cmd
+      [":load"]
+      "<file>"
+      (Compile . CompileFile)
+      "Cargar un programa desde un archivo"
+  , Cmd
+      [":print"]
+      "<exp>"
+      PPrint
+      "Imprime un término y sus ASTs sin evaluarlo"
+  , Cmd
+      [":reload"]
+      ""
+      (const Reload)
+      "Vuelve a cargar el último archivo cargado"
+  , Cmd [":type"] "<exp>" Type "Chequea el tipo de una expresión"
+  , Cmd [":quit", ":Q"] "" (const Quit) "Salir del intérprete"
+  , Cmd [":help", ":?"] "" (const Help) "Mostrar esta lista de comandos"]
 
 helpTxt :: [InteractiveCommand] -> String
-helpTxt cs
-  =  "Lista de comandos:  Cualquier comando puede ser abreviado a :c donde\n" ++
-     "c es el primer caracter del nombre completo.\n\n" ++
-     "<expr>                  evaluar la expresión\n" ++
-     "let <var> = <expr>      definir una variable\n" ++
-     unlines (map (\ (Cmd c a _ d) ->
-                   let  ct = intercalate ", " (map (++ if null a then "" else " " ++ a) c)
-                   in   ct ++ replicate ((24 - length ct) `max` 2) ' ' ++ d) cs)
+helpTxt cs =
+  "Lista de comandos:  Cualquier comando puede ser abreviado a :c donde\n"
+  ++ "c es el primer caracter del nombre completo.\n\n"
+  ++ "<expr>                  evaluar la expresión\n"
+  ++ "let <var> = <expr>      definir una variable\n"
+  ++ unlines
+    (map
+       (\(Cmd c a _ d)
+        -> let ct = intercalate
+                 ", "
+                 (map
+                    (++ if null a
+                        then ""
+                        else " " ++ a)
+                    c)
+           in ct ++ replicate ((24 - length ct) `max` 2) ' ' ++ d)
+       cs)
 
 -- | 'handleCommand' interpreta un comando y devuelve un booleano
 -- indicando si se debe salir del programa o no.
-handleCommand ::  MonadFD4 m => Command  -> m Bool
+handleCommand :: MonadFD4 m => Command -> m Bool
 handleCommand cmd = do
-   s@GlEnv {..} <- get
-   case cmd of
-       Quit   ->  return False
-       Noop   ->  return True
-       Help   ->  printFD4 (helpTxt commands) >> return True
-       Browse ->  do  printFD4 (unlines (reverse (nub (map declName termEnvironment))))
-                      return True
-       Compile c ->
-                  do  case c of
-                          CompileInteractive e -> compilePhrase e
-                          CompileFile f        -> compileFile f
-                      return True
-       Reload ->  eraseLastFileDecls >> (getLastFile >>= compileFile) >> return True
-       PPrint e   -> printPhrase e >> return True
-       Type e    -> typeCheckPhrase e >> return True
+  s@GlEnv { .. } <- get
+  case cmd of
+    Quit      -> return False
+    Noop      -> return True
+    Help      -> printFD4 (helpTxt commands) >> return True
+    Browse    -> do
+      printFD4 (unlines (reverse (nub (map declName termEnvironment))))
+      return True
+    Compile c -> do
+      case c of
+        CompileInteractive e -> compilePhrase e
+        CompileFile f        -> compileFile f
+      return True
+    Reload
+      -> eraseLastFileDecls >> (getLastFile >>= compileFile) >> return True
+    PPrint e  -> printPhrase e >> return True
+    Type e    -> typeCheckPhrase e >> return True
 
-compilePhrase ::  MonadFD4 m => String -> m ()
+compilePhrase :: MonadFD4 m => String -> m ()
 compilePhrase x = do
-    dot <- parseIO "<interactive>" declOrTm x
-    case dot of
-      Left d  -> handleDeclaration d
-      Right t -> handleTerm t
+  dot <- parseIO "<interactive>" declOrTm x
+  case dot of
+    Left d  -> handleDeclaration d
+    Right t -> handleTerm t
 
-handleTerm ::  MonadFD4 m => STerm -> m ()
+handleTerm :: MonadFD4 m => STerm -> m ()
 handleTerm t = do
-         s <- get
-         let t' = elabTerm (globalTypeContext s) t
-         tt <- tc t' (globalTypedEnvironment s)
-         te <- eval tt
-         ppte <- pp te
-         printFD4 (ppte ++ " : " ++ ppTy (getTy tt))
+  s <- get
+  let t' = elabTerm (globalTypeContext s) t
+  tt <- tc t' (globalTypedEnvironment s)
+  te <- eval tt
+  ppte <- pp te
+  printFD4 (ppte ++ " : " ++ ppTy (getTy tt))
 
-printPhrase   :: MonadFD4 m => String -> m ()
-printPhrase x =
-  do
-    x' <- parseIO "<interactive>" tm x
-    s <- get
-    let ex = elabTerm (globalTypeContext s) x'
-    tyenv <- gets globalTypedEnvironment
-    tex <- tc ex tyenv
-    t  <- case x' of
-           (SV p f) -> fromMaybe tex <$> lookupDecl f
-           _       -> return tex
-    printFD4 "STerm:"
-    printFD4 (show x')
-    printFD4 "TTerm:"
-    printFD4 (show t)
+printPhrase :: MonadFD4 m => String -> m ()
+printPhrase x = do
+  x' <- parseIO "<interactive>" tm x
+  s <- get
+  let ex = elabTerm (globalTypeContext s) x'
+  tyenv <- gets globalTypedEnvironment
+  tex <- tc ex tyenv
+  t <- case x' of
+    (SV p f) -> fromMaybe tex <$> lookupDecl f
+    _        -> return tex
+  printFD4 "STerm:"
+  printFD4 (show x')
+  printFD4 "TTerm:"
+  printFD4 (show t)
 
 typeCheckPhrase :: MonadFD4 m => String -> m ()
 typeCheckPhrase x = do
-         t <- parseIO "<interactive>" tm x
-         s <- get
-         let t' = elabTerm (globalTypeContext s) t
-         tt <- tc t' (globalTypedEnvironment s)
-         let ty = getTy tt
-         printFD4 (ppTy ty)
+  t <- parseIO "<interactive>" tm x
+  s <- get
+  let t' = elabTerm (globalTypeContext s) t
+  tt <- tc t' (globalTypedEnvironment s)
+  let ty = getTy tt
+  printFD4 (ppTy ty)

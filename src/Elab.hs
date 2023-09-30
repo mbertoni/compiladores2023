a5a5bc3e108c8402097e65719efd72644253020b
Mariano Bertoni <mariano.bertoni@gmail.com>
2023-09-30 13:17:50 -0300

    Elaboración Artesanal - wip

--- src/Elab.hs
+++ src/Elab.hs
@@ -11,80 +11,133 @@
 --
 -- Este módulo permite elaborar términos y declaraciones para convertirlas desde
 -- fully named (@STerm) a locally closed (@Term@)
-module Elab (elabDeclaration, elabTerm) where
+module Elab where
 
 import Common (abort)
+import Control.Monad
 import Core
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Default
+import Data.List.NonEmpty
 import Data.Maybe
 import Subst
-import qualified Surf as S
-import Data.String (IsString (..))
-
+import Surf qualified as S
 
 -- | 'go transforma variables ligadas en índices de de Bruijn
 -- en un término dado.
-elabTerm :: [(Name, Ty)] -> S.Term -> Term
-elabTerm types = go []
+term :: [Binder] -> S.Term -> Term
+term gamma = term' []
   where
-    go' :: S.Ty -> Ty
-    go' = elabType types
+    ty' :: S.Ty -> Ty
+    ty' = ty gamma
 
-    go :: [Name] -> S.Term -> Term
-    go env = \case
-      S.Var p v ->
-        -- Tenemos que ver si la variable es Global o es un nombre local
-        -- En env llevamos la lista de nombres locales.
-        if v `elem` env
-          then Var p (Free v)
-          else Var p (Global v)
-      S.Lit p int -> Lit p (fromInteger int)
-      S.Lam p [] t -> abort "Empty lambda binding list"
-      S.Lam p [(v, ty)] t -> Lam p v (go' ty) (close v (go (v : env) t))
-      S.Lam p ((v, ty) : bs) t ->
-        Lam p v (go' ty) (close v (go (v : env) (S.Lam p bs t)))
-      S.Fix i (f, fty) [] t -> abort "Empty fix binding list"
-      S.Fix i (f, fty) [(x, xty)] t ->
-        Fix i f (go' fty) x (go' xty) (close2 f x (go (x : f : env) t))
-      S.Fix i (f, fty) ((x, xty) : bs) t ->
-        Fix i f (go' fty) x (go' xty) (close2 f x (go (x : f : env) (S.Lam i bs t)))
-      S.IfZ p c t e -> IfZ p (go env c) (go env t) (go env e)
-      -- des hardcodear el Bang
-      S.LetFun i (fn, ty) bs t t' ->
+    binder' = binder gamma
+
+    multi' = multi gamma
+
+    term' :: [Name] -> S.Term -> Term
+    term' locals (S.T tm) = case tm of
+      -- Tenemos que ver si la variable es Global o es un nombre local
+      -- En locals llevamos la lista de nombres locales.
+      S.Var i ->
+        let nm = ident i
+        in if nm `elem` locals
+            then Var def (Free nm)
+            else Var def (Global nm)
+      S.Lit l -> Lit def (literal l)
+      S.Pnt l t -> Pnt def (literal l) (rec t)
+      S.UOp op t -> case op of
+        S.Bang -> IfZ def (rec t) 1 0
+      S.BOp op t u -> BOp def (binaryOp op) (rec t) (rec u)
+      S.IfZ c t e -> IfZ def (rec c) (rec t) (rec e)
+      S.App f x -> App def (rec f) (rec x)
+      S.Fun bs t -> f locals (toList bs >>= multi')
+        where
+          -- TODO: f :: [Name] -> NonEmpty Binder -> Term
+          -- el env de local se pasa con foldl
+          -- el term de lam anidadas con foldr
+          -- se puede separar en dos pasadas, una que elab y la otra que
+          -- pasa a locally closed.
+          -- Se puede hacer tuplin? Ver el último libro de bird );
+          f :: [Name] -> [Binder] -> Term
+          f e [] = term' e t
+          f e ((x, tau) : xts) = Lam def x tau (close x (f (x : e) xts))
+      S.Fix f x bs t -> Fix def f' tf' x' tx' scope2
+        where
+          (f', tf') = binder' f
+          (x', tx') = binder' x
+          scope2 = close2 f' x' (rec t')
+          t' = case bs of
+            [] -> t
+            _ -> S.T $ S.Fun (fromList bs) t
+      _ -> abort "wip"
+      where
+        rec = term' locals
+
+{-
+      S.Lam [(v, ty)] t -> Lam p v (go' ty) (close v (go (v : env) t))
+      S.Lam ((v, ty) : bs) t ->
+        Lam v (go' ty) (close v (go (v : env) (S.Lam p bs t)))
+      S.Fix (f, fty) [(x, xty)] t ->
+        Fix f (go' fty) x (go' xty) (close2 f x (go (x : f : env) t))
+      S.Fix (f, fty) ((x, xty) : bs) t ->
+        Fix f (go' fty) x (go' xty) (close2 f x (go (x : f : env) (S.Lam i bs t)))
+      S.Let (fn, ty) bs t t' ->
         go env (S.Let i (fn, funTy) (S.Lam i bs t) t')
         where
           funTy = S.tyFold (map snd bs ++ [ty])
-      S.UOp i S.Bang t ->
-        IfZ i (go env t) (Lit i (N 1)) (Lit i (N 0))
-      S.If i _ -> abort "unimplemented"
+
       -- Operadores binarios
-      S.BOp i o t u -> BOp i (elabBOp o) (go env t) (go env u)
+
       -- Operador Print
-      S.Pnt i str t -> Pnt i (fromString str) (go env t)
       -- Aplicaciones generales
-      S.App p h a -> App p (go env h) (go env a)
-      S.Let p (v, vty) def bdy ->
-        Let p v (go' vty) (go env def) (close v (go (v : env) bdy))
-      S.LetRec i (f, ty) [] t t' -> abort "Empty let rec list"
-      S.LetRec i (f, ty) (b : bs) t t' ->
-        go env (S.Let i (f, funTy) (S.Fix i (f, funTy) (b : bs) t) t')
+
+      S.Let (v, vty) def bdy ->
+        Let def v (go' vty) (go env def) (close v (go (v : env) bdy))
+      S.Let (f, ty) (b : bs) t t' ->
+        go env (S.Let (f, funTy) (S.Fix (f, funTy) (b : bs) t) t')
         where
           funTy = S.tyFold (map snd (b : bs) ++ [ty])
+-}
 
-elabType :: [(Name, Ty)] -> S.Ty -> Ty
-elabType types = \case
+ident :: S.Ident -> Name
+ident = \case
+  S.VarId s -> s
+  S.TyId s -> s
+
+binder :: [Binder] -> S.Binder -> Binder
+binder gamma = bimap ident (ty gamma)
+
+multi :: [Binder] -> S.Multi -> [Binder]
+multi gamma (is, tau) = toList $ fmap (\i -> (ident i, ty gamma tau)) is
+
+ty :: [Binder] -> S.Ty -> Ty
+ty gamma = \case
   S.Nat -> Nat
-  S.Arrow t t' -> Arrow (elabType types t) (elabType types t')
-  S.Alias n -> fromMaybe (abort "alias no definido") (lookup n types)
+  S.ParTy t -> r t
+  S.Arrow t t' -> Arrow (r t) (r t')
+  S.Alias n -> fromMaybe (abort "alias no definido") (lookup (ident n) gamma)
+  where
+    r = ty gamma
 
+literal :: S.Literal -> Literal
+literal = \case
+  S.N n -> N $ fromInteger n
+  S.S s -> S s
 
-elabBOp :: S.BinaryOp -> BinaryOp
-elabBOp S.Add = Add
-elabBOp S.Sub = Sub
+binaryOp :: S.BinaryOp -> BinaryOp
+binaryOp S.Add = Add
+binaryOp S.Sub = Sub
 
+tyFold :: [Ty] -> Ty
+tyFold = foldr1 Arrow
+
+{-
 elabDeclaration :: [(Name, Ty)] -> S.Declaration -> Decl (Either Term Ty)
 elabDeclaration types decl =
   Decl {name = S.name decl, pos = S.pos decl, body = elaboratedBody}
   where
     elaboratedBody = case S.body decl of
-      S.TermDecl sTerm -> Left $ elabTerm types sTerm
+      S.LetDecl sTerm -> Left $ elabTerm types sTerm
       S.TypeDecl sType -> Right $ elabType types sType
+-}
